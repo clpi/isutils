bfb2c8840b6709a0cdc65f368714a713d556892e
pecusys <chrisp1877@gmail.com>
2020-09-18 23:07:48 -0700

    adding build script

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 """
+python-docx
+mutagen
+pillow
+lxml
 TODO Consider *drastically* slimming down demo, section, step models so as to only act as lxml etree wrappers
 """
 from ui.window import MainWindow, run
