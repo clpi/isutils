61fb32df01d3b6aa2785a84cc7bca4aa0c8ca9e3
Chris P <clp@clp.is>
2022-04-03 02:37:24 -0700

    d

--- .qt_for_python/uic/wind.py
+++ .qt_for_python/uic/wind.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'win.ui'
+## Form generated from reading UI file 'wind.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.2.4
 ##
